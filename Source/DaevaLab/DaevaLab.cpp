f7a37f2e314d0ac5b0ebd5dc707f05ab57637bb9
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2025-08-02 21:41:12 -0500

    feat: initial commit

---
+++ Source/DaevaLab/DaevaLab.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DaevaLab.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DaevaLab, "DaevaLab" );
